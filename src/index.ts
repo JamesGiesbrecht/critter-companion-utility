74eccc72dff546b449488cd14aec08703ed80f13
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-08-21 11:48:11 -0500

    Setting up exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as Types from 'ts/types'
+export { default as bugsData } from 'data/bugs'
+export { default as fishData } from 'data/fish'
+export { default as seaData } from 'data/sea'
 
-export default {
-  Types,
-}
+export * from 'ts/types'
