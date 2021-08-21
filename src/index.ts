e583254af5bdf956f194df6f892e080938afc2a6
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-08-21 12:50:30 -0500

    Updating tsConfig

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-export { default as bugsData } from 'data/bugs'
-export { default as fishData } from 'data/fish'
-export { default as seaData } from 'data/sea'
+export { default as bugsData } from './data/bugs'
+export { default as fishData } from './data/fish'
+export { default as seaData } from './data/sea'
 
-export * from 'ts/types'
+export * from './ts/types'
--- src/ts/types.ts
+++ src/ts/types.ts
@@ -2,31 +2,8 @@ export type CritterSize = 'Tiny' | 'Small' | 'Medium' | 'Large' | 'X-Large' | 'H
 export type CritterSpeed = 'Stationary' | 'Very slow' | 'Slow' | 'Medium' | 'Fast' | 'Very fast'
 
 export type CritterType = 'Bugs' | 'Fish' | 'Sea'
-export type Hour =
-  | 1
-  | 2
-  | 3
-  | 4
-  | 5
-  | 6
-  | 7
-  | 8
-  | 9
-  | 10
-  | 11
-  | 12
-  | 13
-  | 14
-  | 15
-  | 16
-  | 17
-  | 18
-  | 19
-  | 20
-  | 21
-  | 22
-  | 23
-  | 24
+// eslint-disable-next-line prettier/prettier
+export type Hour = 1| 2| 3| 4| 5| 6| 7| 8| 9| 10| 11| 12| 13| 14| 15| 16| 17| 18| 19| 20| 21| 22| 23| 24
 export type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
 
 export type BaseCritter = {
