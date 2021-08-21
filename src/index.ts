179922d96a0ed41c6e9f6ef81d4a176feea17ec2
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-08-21 17:12:35 -0500

    Fixing root exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from 'data/bugs'
-export * from 'data/fish'
-export * from 'data/sea'
-export * from 'ts/types'
+export { default as bugsData } from './data/bugs'
+export { default as fishData } from './data/fish'
+export { default as seaData } from './data/sea'
+export * from './ts/types'
