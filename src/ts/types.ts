7520996b0fc50f06dbb5c1af63888b9db4cecbd5
JamesGiesbrecht <giesbrechtjames@gmail.com>
2021-08-22 13:16:07 -0500

    Fixing enums for export

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -1,5 +1,3 @@
-import { Statuses } from './enums'
-
 export type CritterSize = 'Tiny' | 'Small' | 'Medium' | 'Large' | 'X-Large' | 'Huge' | 'Long'
 export type CritterSpeed = 'Stationary' | 'Very slow' | 'Slow' | 'Medium' | 'Fast' | 'Very fast'
 
@@ -7,6 +5,7 @@ export type CritterType = 'Bugs' | 'Fish' | 'Sea'
 // eslint-disable-next-line prettier/prettier
 export type Hour = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 | 23 | 24
 export type Month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12
+export type Status = 'new' | 'leaving' | 'incoming'
 
 export interface BaseCritter {
   id: string
@@ -23,8 +22,8 @@ export interface BaseCritter {
 }
 export type Critter = BaseCritter & {
   isAvailableNow?: boolean
-  [Statuses.Leaving]?: boolean
-  [Statuses.New]?: boolean
-  [Statuses.Incoming]?: boolean
+  isLeaving?: boolean
+  isNew?: boolean
+  isIncoming?: boolean
   months: Month[]
 }
